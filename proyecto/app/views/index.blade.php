226db957bbbc4d6fb506433c51a607debcc89b39
Juan Carlos <quintanilla.peru@gmail.com>
2015-02-28 19:23:49 -0500

    ajustes

--- proyecto/app/views/index.blade.php
+++ proyecto/app/views/index.blade.php
@@ -18,5 +18,55 @@
 		</p>
 	</div>
 </section>
-
+<h2 class="text-center">
+	Todo lo que necesitas para vender en internet.
+	<br><br>
+	<small>
+		La forma más sencilla y rápida de crear tu comercio electrónico,<br>
+		Productoonline.es tiene todo lo que necesitas.
+	</small>
+</h2>
+<br>
+<section class="row">
+	<div class="col-sm-3 text-center">
+		<div class="borde-circulo center-block">
+			<img class="img-responsive" src="{{url()}}/images/iconos/productos.png" alt="">
+		</div>
+		<br>
+		Añade y edita productos
+		<br>
+		<img src="{{url()}}/images/iconos/icono-mas.png" alt="">
+		<br>
+	</div>
+	<div class="col-sm-3 text-center">
+		<div class="borde-circulo center-block">
+			<img class="img-responsive center-block" src="{{url()}}/images/iconos/categorias.png" alt="">
+		</div>
+		<br>
+		Añade categorías
+		<br>
+		<img src="{{url()}}/images/iconos/icono-mas.png" alt="">
+		<br>
+	</div>
+	<div class="col-sm-3 text-center">
+		<div class="borde-circulo center-block">
+			<img class="img-responsive center-block" src="{{url()}}/images/iconos/ventas.png" alt="">
+		</div>
+		<br>
+		Controla tus ventas
+		<br>
+		<img src="{{url()}}/images/iconos/icono-mas.png" alt="">
+		<br>
+	</div>
+	<div class="col-sm-3 text-center">
+		<div class="borde-circulo center-block">
+			<img class="img-responsive center-block" src="{{url()}}/images/iconos/clientes.png" alt="">
+		</div>
+		<br>
+		Ver clientes y pedidos online
+		<br>
+		<img src="{{url()}}/images/iconos/icono-mas.png" alt="">
+		<br>
+	</div>
+</section>
 @stop
