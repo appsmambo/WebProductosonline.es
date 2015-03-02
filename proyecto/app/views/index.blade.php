8066efc0ed9b2001d3e8a182de30d10a1969bc2b
appsmambo <quintanilla.peru@gmail.com>
2015-03-02 18:42:53 -0500

    mas del home

--- proyecto/app/views/index.blade.php
+++ proyecto/app/views/index.blade.php
@@ -26,7 +26,7 @@
 		Productoonline.es tiene todo lo que necesitas.
 	</small>
 </h2>
-<br>
+<p class="hidden-xs"><br><br></p>
 <section class="row">
 	<div class="col-sm-3 text-center">
 		<div class="borde-circulo center-block">
@@ -36,7 +36,6 @@
 		Añade y edita productos
 		<br>
 		<img src="{{url()}}/images/iconos/icono-mas.png" alt="">
-		<br>
 	</div>
 	<div class="col-sm-3 text-center">
 		<div class="borde-circulo center-block">
@@ -46,7 +45,6 @@
 		Añade categorías
 		<br>
 		<img src="{{url()}}/images/iconos/icono-mas.png" alt="">
-		<br>
 	</div>
 	<div class="col-sm-3 text-center">
 		<div class="borde-circulo center-block">
@@ -56,7 +54,6 @@
 		Controla tus ventas
 		<br>
 		<img src="{{url()}}/images/iconos/icono-mas.png" alt="">
-		<br>
 	</div>
 	<div class="col-sm-3 text-center">
 		<div class="borde-circulo center-block">
@@ -66,7 +63,72 @@
 		Ver clientes y pedidos online
 		<br>
 		<img src="{{url()}}/images/iconos/icono-mas.png" alt="">
-		<br>
 	</div>
 </section>
+<p><br></p>
+<p>
+	Confiamos en nuestro servicio, por eso queremos que lo pruebes antes de contratarlo.
+</p>
+<p><br></p>
+<section class="row fondo-azul">
+	<div class="col-sm-10 col-sm-offset-1">
+		<div class="row">
+			<div class="col-sm-4">
+				<div class="row">
+					<div class="col-sm-3">
+						<div class="borde-circulo-blanco">1</div>
+					</div>
+					<div class="col-sm-9 blanco">
+						<p class="blanco fs-16">
+							Contrata el perido de prueba de un mes, sin permanencia.
+						</p>
+					</div>
+				</div>
+			</div>
+			<div class="col-sm-4">
+				<div class="row">
+					<div class="col-sm-3">
+						<div class="borde-circulo-blanco">2</div>
+					</div>
+					<div class="col-sm-9">
+						<p class="blanco fs-16">
+							Un técnico te guiará en todo el proceso de la creación de tu tienda online.
+						</p>
+					</div>
+				</div>
+			</div>
+			<div class="col-sm-4">
+				<div class="row">
+					<div class="col-sm-3">
+						<div class="borde-circulo-blanco">3</div>
+					</div>
+					<div class="col-sm-9">
+						<p class="blanco fs-16">
+							Si no quedas conforme te daremos de baja sin coste.
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-sm-4 col-sm-offset-4">
+				<br>
+				<button type="button" class="center-block btn btn-default">Comenzar periodo de prueba</button>
+			</div>
+			<div class="col-sm-4">
+				<span class="hidden-xs"><br><br></span>
+				<p class="blanco text-right fs-12">
+					30 días - Sin datos bancarios - Sin Permanencia
+				</p>
+			</div>
+		</div>
+	</div>
+</section>
+<p><br></p>
+<h2 class="text-center">
+	"sin necesidad de conocimientos técnicos"
+</h2>
+<p class="hidden-xs"><br><br></p>
+
+
 @stop
